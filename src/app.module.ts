2e76ca838a9427cdcaca32f57c3b105ad5fd82fb
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-19 22:01:55 +0300

    update db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: +process.env.MYSQL_PORT,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DB,
+      entities: ["dist/**/*.entity.js"],
+      synchronize: true,
+      autoLoadEntities: true
+    }),
+    UsersModule, 
+    AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
