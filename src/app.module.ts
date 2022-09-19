76d6ee00a9198717940ca0616d4a5e20ee66c5f8
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-19 22:05:23 +0300

    create posts resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
@@ -23,7 +24,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true
     }),
     UsersModule, 
-    AuthModule],
+    AuthModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
