29e4f4195c4e4b290122d5fa44fa7953a8e049ad
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-20 13:41:05 +0300

    delete unnecessary  files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
@@ -24,8 +22,6 @@ import { PostsModule } from './posts/posts.module';
       autoLoadEntities: true
     }),
     UsersModule, 
-    AuthModule, PostsModule],
-  controllers: [AppController],
-  providers: [AppService],
+    AuthModule, PostsModule]
 })
 export class AppModule {}
