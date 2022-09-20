65b4e6ad44c7b19f53b8a386e82a583c2b7c8907
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-21 00:44:55 +0300

    Create account activation guard

---
+++ src/auth/account-activation.guard.ts
@@ -0,0 +1,29 @@
+import { CanActivate, ExecutionContext, HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { User } from "src/users/entities/user.entity";
+import { UserDataBaseService } from "src/users/helpers/db.helper";
+
+@Injectable()
+export class AccountActivationGuard implements CanActivate {
+    constructor(private userDataBaseService: UserDataBaseService,){}
+
+    async canActivate(context: ExecutionContext): Promise<boolean>  {
+        const request = context.switchToHttp().getRequest()
+        try {
+            const user: User = await this.userDataBaseService.findUserByEmail(request.body.email)
+            if(!user.activated){
+                throw new HttpException({
+                    status: HttpStatus.FORBIDDEN,
+                    error: 'account is not activated'
+                }, HttpStatus.FORBIDDEN)
+            }
+
+            return true
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.FORBIDDEN,
+                error: 'account is not activated'
+            }, HttpStatus.FORBIDDEN)
+        }
+    }
+}
