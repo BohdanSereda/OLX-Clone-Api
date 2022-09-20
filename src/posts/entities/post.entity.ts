78a61718636fda2cc50d5cebc9293b0e7c2e7754
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-21 00:47:56 +0300

    create post entity

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,1 +1,40 @@
-export class Post {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Post {
+    @ApiProperty({example: 1,  type: Number, description: 'post id'})
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @ApiProperty({example: 'Car',  type: String, description: 'post name'})
+    @Column({nullable: false, default: ''})
+    name: string
+
+    
+    @ApiProperty({example: '......',  type: String, description: 'post images'})
+    @Column()
+    category: string
+
+    @ApiProperty({example: '......',  type: [String], description: 'post images'})
+    @Column("simple-array", {nullable: true})
+    images: string[]
+
+
+    @ApiProperty({example: 'Very cool car',  type: String, description: 'post description'})
+    @Column("longtext", {nullable: true})
+    description: string
+
+    @ApiProperty({example: 28000,  type: Number, description: 'post price'})
+    @Column()
+    price: number
+
+    @Column()
+    condition: string
+
+    @Column()
+    address: string
+
+    @Column()
+    phone: string
+}
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Post]),
+    AuthModule, 
+    UsersModule
+  ],
   controllers: [PostsController],
   providers: [PostsService]
 })
