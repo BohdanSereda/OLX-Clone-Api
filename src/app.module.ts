81abcce3dd4bf62e3189901ac151ce72ba7481df
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-20 20:01:02 +0300

    create mail sending logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModule } from './posts/posts.module';
+import { MailModule } from './mail/mail.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
@@ -21,7 +23,16 @@ import { PostsModule } from './posts/posts.module';
       synchronize: true,
       autoLoadEntities: true
     }),
+    MailerModule.forRoot({
+      transport:{
+        host: process.env.MAILER_HOST,
+        auth:{
+          user: process.env.MAILER_USER,
+          pass: process.env.MAILER_PASSWORD
+        }
+      }
+    }),
     UsersModule, 
-    AuthModule, PostsModule]
+    AuthModule, PostsModule, MailModule]
 })
 export class AppModule {}
---
+++ src/mail/mail.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { MailerService } from "@nestjs-modules/mailer";
+import { User } from 'src/users/entities/user.entity';
+
+@Injectable()
+
+export class MailService {
+    constructor(
+        private readonly mailerService: MailerService
+    ) { }
+    async sendActivationEmail(user: User): Promise<void> {
+        console.log(user)
+        const text = `Hi user please confirm your email via link\n${process.env.API_URL}/auth/activate/${user.activationLink}`
+        const from = 'scraper.api.study@gmail.com'
+        const subject = "Confirmation"
+        await this.sendEmail(from, subject, user.email, text)
+    }
+    async sendEmail( from: string, subject: string, to: string, text: string): Promise<void>{
+        await this.mailerService.sendMail({ to, from, subject, text })
+    }
+}
