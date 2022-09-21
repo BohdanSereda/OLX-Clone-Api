94c489376c986d10650c3258b4abe59729a5e5bb
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-22 00:07:50 +0300

    created dtos and custom types

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,39 @@
-export class CreatePostDto {}
+import { ApiProperty } from "@nestjs/swagger"
+import { Type } from "class-transformer"
+import { IsString, IsNotEmpty, IsIn, IsNumber, IsPhoneNumber } from "class-validator"
+import { CategoryType } from "./custom-types.dto"
+
+export class CreatePostDto {
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({example: 'Car',  type: String, description: 'good\'s name'})
+    name: string
+
+    @IsString()
+    @IsNotEmpty()
+    @IsIn(['pets', 'cars', 'electronics', 'realty','clothing', 'other'])
+    @ApiProperty({example: 'cars',  type: "CategoryType", description: 'good\'s category'})
+    category: CategoryType
+
+    @IsString()
+    @ApiProperty({example: 'Very cool car',  type: String, description: 'good\'s description'})
+    description: string
+
+    @Type(() => Number)
+    @IsNumber()
+    @ApiProperty({example: 28000,  type: Number, description: 'good\'s price'})
+    price: number
+
+    @IsString()
+    @ApiProperty({example: 'New',  type: String, description: 'good\'s condition'})
+    condition: string
+
+    @IsString()
+    @ApiProperty({example: 'Lviv, Naukova 36/9',  type: String, description: 'user\'s address'})
+    address: string
+
+    @IsString()
+    @IsPhoneNumber()
+    @ApiProperty({example: '+380734162069',  type: String, description: 'user\'s phone number'})
+    phone: string
+}
---
+++ src/posts/dto/custom-types.dto.ts
@@ -0,0 +1,2 @@
+export const fileFilter = 'raw|png|jpg|jpeg|tiff|psd|bmp|gif'
+export type CategoryType = 'pets' | 'cars' | 'electronics' | 'realty' | 'clothing' | 'other'
