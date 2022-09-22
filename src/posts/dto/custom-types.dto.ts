7ff8afa67425775fca4e338d4d766a2a3fa69c7b
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-23 00:11:19 +0300

    create new custom types

--- src/posts/dto/custom-types.dto.ts
+++ src/posts/dto/custom-types.dto.ts
@@ -1,2 +1,16 @@
+import { FindOperator } from "typeorm";
+
 export const fileFilter = 'raw|png|jpg|jpeg|tiff|psd|bmp|gif'
 export type CategoryType = 'pets' | 'cars' | 'electronics' | 'realty' | 'clothing' | 'other'
+
+export type CustomQueryType = {
+    where?:Where
+};
+
+export type Where = {
+    price?: FindOperator<number>,
+    name?: FindOperator<string>,
+    description?: FindOperator<string>,
+    address?: FindOperator<string>,
+    condition?: string
+}
