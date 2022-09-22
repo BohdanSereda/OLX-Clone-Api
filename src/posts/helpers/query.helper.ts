2a5e0c6a87828ec527ef5166626107a79e57805f
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-23 00:11:31 +0300

    create query builder

---
+++ src/posts/helpers/query.helper.ts
@@ -0,0 +1,42 @@
+import { Between, LessThan, Like, MoreThan } from "typeorm"
+import { CustomQueryType } from "../dto/custom-types.dto"
+
+
+
+export class QueryHelper{
+    static queryBuilder(query){
+        const resultQuery: CustomQueryType ={
+            where:{
+                
+            }
+        }
+        if(query.minPrice && query.maxPrice){
+            resultQuery.where = Object.assign(resultQuery.where, {price: Between(query.minPrice, query.maxPrice)})
+        }  
+        if (query.minPrice && !query.maxPrice) {
+            resultQuery.where = Object.assign(resultQuery.where, {price: MoreThan(query.minPrice)})
+        }  
+        if (query.maxPrice && !query.minPrice) {
+            resultQuery.where = Object.assign(resultQuery.where, {price: LessThan(query.maxPrice)})
+        }
+        
+        if(query.name){
+            resultQuery.where = Object.assign(resultQuery.where, {name: Like(`%${query.name}%`)})
+        }
+
+        if (query.description) {
+            resultQuery.where = Object.assign(resultQuery.where, {description: Like(`%${query.description}%`)})
+        }
+
+        if (query.address) {
+            resultQuery.where = Object.assign(resultQuery.where, {address: Like(`%${query.address}%`)})
+        }
+
+        if (query.condition) {
+            resultQuery.where = Object.assign(resultQuery.where, {condition: query.condition})
+        }
+        console.log(resultQuery);
+        
+        return resultQuery
+    }
+}
