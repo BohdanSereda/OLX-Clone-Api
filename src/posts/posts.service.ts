3b074a592a89e701c60e3c634edf6d9db9449eac
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-23 00:10:46 +0300

    update posts service

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -4,30 +4,29 @@ import { CreatePostDto } from './dto/create-post.dto';
 import { IGetUserAuthInfoRequest } from './dto/custom-request.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PostDataBaseService } from './posts.database.service';
-import { S3Service } from './S3.service';
 
 
 
 @Injectable()
 export class PostsService {
   constructor(@Inject(REQUEST) private readonly request: IGetUserAuthInfoRequest,
-              private readonly s3Service: S3Service,
               private readonly postDataBaseService: PostDataBaseService){}
 
   
   async create(images: Array<Express.Multer.File>, createPostDto: CreatePostDto) {
     const user = this.request.user    
-    const post = await this.postDataBaseService.createPost(createPostDto, user, images)
-    return post
+    return await this.postDataBaseService.createPost(createPostDto, user, images)
+     
   }
 
-  async findAll() {
+  async findUserPosts() {
     const user = this.request.user  
     return await this.postDataBaseService.findPostsByUserId(user.id)
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  async deactivate(postId: number) {
+    const user = this.request.user  
+    return  await this.postDataBaseService.deactivate(postId, user.id)
   }
 
   async update(postId: number, updatePostDto: UpdatePostDto, images) {
@@ -35,7 +34,12 @@ export class PostsService {
     return await this.postDataBaseService.updatePost(updatePostDto, postId, user.id, images)
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  async remove(postId: number) {
+    const user = this.request.user  
+    return await this.postDataBaseService.removePost(postId, user.id)
+  }
+
+  async findAll(query){
+    return await this.postDataBaseService.findAllPosts(query)
   }
 }
