17a25f2e01d0d1caa8d7a78d9f28467623003769
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-24 13:50:35 +0300

    update error message

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -45,7 +45,7 @@ export class PostsService {
       if (!post) {
         throw new HttpException({
           status: HttpStatus.NOT_FOUND,
-          error: `can't find post with this id: ${postId}`,
+          error: `can't get post with this id: ${postId}`,
         }, HttpStatus.NOT_FOUND)
       }
       return post
