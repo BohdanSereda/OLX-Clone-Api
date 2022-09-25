3c06e3b419d24432c584bffaa07fb542e36dc3f2
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-26 00:13:18 +0300

    refactor

--- src/posts/helpers/query.helper.ts
+++ src/posts/helpers/query.helper.ts
@@ -1,40 +1,52 @@
-import { Between, LessThan, Like, MoreThan } from "typeorm"
-import { CustomQueryType } from "../dto/custom-types.dto"
+import { Between, LessThan, Like, MoreThan } from 'typeorm';
+import { CustomQueryType } from '../dto/custom-types.dto';
+import { QueryFilterDto } from '../dto/post-query.dto';
 
-
-
-export class QueryHelper{
-    static queryBuilder(query){
-        const resultQuery: CustomQueryType ={
-            where:{
-                
-            }
+export class QueryHelper {
+    static queryBuilder(query: QueryFilterDto) {
+        const resultQuery: CustomQueryType = {
+            relations: { user: true },
+            where: {},
+        };
+        if (query.minPrice && query.maxPrice) {
+            resultQuery.where = Object.assign(resultQuery.where, {
+                price: Between(query.minPrice, query.maxPrice),
+            });
         }
-        if(query.minPrice && query.maxPrice){
-            resultQuery.where = Object.assign(resultQuery.where, {price: Between(query.minPrice, query.maxPrice)})
-        }  
         if (query.minPrice && !query.maxPrice) {
-            resultQuery.where = Object.assign(resultQuery.where, {price: MoreThan(query.minPrice)})
-        }  
-        if (query.maxPrice && !query.minPrice) {
-            resultQuery.where = Object.assign(resultQuery.where, {price: LessThan(query.maxPrice)})
+            resultQuery.where = Object.assign(resultQuery.where, {
+                price: MoreThan(query.minPrice),
+            });
         }
-        
-        if(query.name){
-            resultQuery.where = Object.assign(resultQuery.where, {name: Like(`%${query.name}%`)})
+        if (query.maxPrice && !query.minPrice) {
+            resultQuery.where = Object.assign(resultQuery.where, {
+                price: LessThan(query.maxPrice),
+            });
+        }
+
+        if (query.name) {
+            resultQuery.where = Object.assign(resultQuery.where, {
+                name: Like(`%${query.name}%`),
+            });
         }
 
         if (query.description) {
-            resultQuery.where = Object.assign(resultQuery.where, {description: Like(`%${query.description}%`)})
+            resultQuery.where = Object.assign(resultQuery.where, {
+                description: Like(`%${query.description}%`),
+            });
         }
 
         if (query.address) {
-            resultQuery.where = Object.assign(resultQuery.where, {address: Like(`%${query.address}%`)})
+            resultQuery.where = Object.assign(resultQuery.where, {
+                address: Like(`%${query.address}%`),
+            });
         }
 
         if (query.condition) {
-            resultQuery.where = Object.assign(resultQuery.where, {condition: query.condition})
+            resultQuery.where = Object.assign(resultQuery.where, {
+                condition: query.condition,
+            });
         }
-        return resultQuery
+        return resultQuery;
     }
 }
