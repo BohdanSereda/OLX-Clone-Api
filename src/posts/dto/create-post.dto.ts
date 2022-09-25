7ffe74b94cf39c11546627b6aceba043062e0786
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-26 00:11:37 +0300

    formatted

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,41 +1,72 @@
-import { ApiProperty } from "@nestjs/swagger"
-import { Type } from "class-transformer"
-import { IsString, IsNotEmpty, IsIn, IsNumber, IsPhoneNumber,  Min } from "class-validator"
-import { CategoryType } from "./custom-types.dto"
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+    IsString,
+    IsNotEmpty,
+    IsIn,
+    IsNumber,
+    IsPhoneNumber,
+    Min,
+} from 'class-validator';
+import { CategoryType } from './custom-types.dto';
 
 export class CreatePostDto {
     @IsString()
     @IsNotEmpty()
-    @ApiProperty({example: 'Car',  type: String, description: 'good\'s name'})
-    name: string
+    @ApiProperty({
+        example: 'Car',
+        type: String,
+        description: "good's name",
+    })
+    name: string;
 
     @IsString()
     @IsNotEmpty()
-    @IsIn(['pets', 'cars', 'electronics', 'realty','clothing', 'other'])
-    @ApiProperty({example: 'cars',  type: "CategoryType", description: 'good\'s category'})
-    category: CategoryType
+    @IsIn(['pets', 'cars', 'electronics', 'realty', 'clothing', 'other'])
+    @ApiProperty({
+        example: 'cars',
+        type: 'CategoryType',
+        description: "good's category",
+    })
+    category: CategoryType;
 
     @IsString()
-    @ApiProperty({example: 'Very cool car',  type: String, description: 'good\'s description'})
-    description: string
-    
+    @ApiProperty({
+        example: 'Very cool car',
+        type: String,
+        description: "good's description",
+    })
+    description: string;
+
     @IsNotEmpty()
     @Type(() => Number)
     @IsNumber()
     @Min(0)
-    @ApiProperty({example: 28000,  type: Number, description: 'good\'s price'})
-    price: number
+    @ApiProperty({ example: 28000, type: Number, description: "good's price" })
+    price: number;
 
     @IsString()
-    @ApiProperty({example: 'New',  type: String, description: 'good\'s condition'})
-    condition: string
+    @ApiProperty({
+        example: 'New',
+        type: String,
+        description: "good's condition",
+    })
+    condition: string;
 
     @IsString()
-    @ApiProperty({example: 'Lviv, Naukova 36/9',  type: String, description: 'user\'s address'})
-    address: string
+    @ApiProperty({
+        example: 'Lviv, Naukova 36/9',
+        type: String,
+        description: "user's address",
+    })
+    address: string;
 
     @IsString()
     @IsPhoneNumber()
-    @ApiProperty({example: '+380734162069',  type: String, description: 'user\'s phone number'})
-    phone: string
+    @ApiProperty({
+        example: '+380734162069',
+        type: String,
+        description: "user's phone number",
+    })
+    phone: string;
 }
