1793cf405175bf8cdc2c05ec9e8f8b2a50c6211c
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-26 00:11:12 +0300

    refactored

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -2,109 +2,166 @@ import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
 import { CreatePostDto } from './dto/create-post.dto';
 import { IGetUserAuthInfoRequest } from './dto/custom-request.dto';
+import { QueryFilterDto } from './dto/post-query.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { Post } from './entities/post.entity';
 import { PostDataBaseService } from './posts.database.service';
 
-
-
 @Injectable()
 export class PostsService {
-  constructor(@Inject(REQUEST) private readonly request: IGetUserAuthInfoRequest,
-    private readonly postDataBaseService: PostDataBaseService) { }
+    constructor(
+        @Inject(REQUEST) private readonly request: IGetUserAuthInfoRequest,
+        private readonly postDataBaseService: PostDataBaseService,
+    ) {}
 
-  async create(images: Array<Express.Multer.File>, createPostDto: CreatePostDto): Promise<Post> {
-    try {
-      const user = this.request.user
-      return await this.postDataBaseService.createPost(createPostDto, user, images)
-    } catch (error) {
-      throw new HttpException({
-        status: HttpStatus.INTERNAL_SERVER_ERROR,
-        error: `something went wrong :(`,
-      }, HttpStatus.INTERNAL_SERVER_ERROR)
-    }
-  }
-
-  async findUserPosts(): Promise<Post[]> {
-    try {
-      const user = this.request.user
-      return await this.postDataBaseService.findPostsByUserId(user.id)
-    } catch (error) {
-      throw new HttpException({
-        status: HttpStatus.INTERNAL_SERVER_ERROR,
-        error: `something went wrong :(`,
-      }, HttpStatus.INTERNAL_SERVER_ERROR)
-    }
-
-  }
-
-  async deactivate(postId: number): Promise<Post> {
-    try {
-      const user = this.request.user
-      const post = await this.postDataBaseService.deactivate(postId, user.id)
-      if (!post) {
-        throw new HttpException({
-          status: HttpStatus.NOT_FOUND,
-          error: `can't get post with this id: ${postId}`,
-        }, HttpStatus.NOT_FOUND)
-      }
-      return post
-    } catch (error) {
-      throw new HttpException({
-        status: error.status || HttpStatus.INTERNAL_SERVER_ERROR,
-        error: error.response.error || `something went wrong :(`,
-      }, error.status || HttpStatus.INTERNAL_SERVER_ERROR)
+    async create(
+        images: Array<Express.Multer.File>,
+        createPostDto: CreatePostDto,
+    ): Promise<Post | false> {
+        try {
+            const user = this.request.user;
+            const post = await this.postDataBaseService.createPost(
+                createPostDto,
+                user,
+                images,
+            );
+            if (!post) {
+                throw new HttpException(
+                    {
+                        status: HttpStatus.CONFLICT,
+                        error: `there is already post with this name`,
+                    },
+                    HttpStatus.CONFLICT,
+                );
+            }
+            return post;
+        } catch (error) {
+            throw new HttpException(
+                {
+                    status: error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+                    error: error.response.error || `something went wrong :(`,
+                },
+                error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
     }
 
-  }
-
-  async update(postId: number, updatePostDto: UpdatePostDto, images: Array<Express.Multer.File>): Promise<Post> {
-    try {
-      const user = this.request.user
-      const post = await this.postDataBaseService.updatePost(updatePostDto, postId, user.id, images)
-      if (!post) {
-        throw new HttpException({
-          status: HttpStatus.NOT_FOUND,
-          error: `can't get post with this id: ${postId}`,
-        }, HttpStatus.NOT_FOUND)
-      }
-      return post
-    } catch (error) {
-      throw new HttpException({
-        status: error.status || HttpStatus.INTERNAL_SERVER_ERROR,
-        error: error.response.error || `something went wrong :(`,
-      }, error.status || HttpStatus.INTERNAL_SERVER_ERROR)
-    }
-  }
-
-  async remove(postId: number): Promise<Post> {
-    try {
-      const user = this.request.user
-      const post = await this.postDataBaseService.removePost(postId, user.id)
-      if (!post) {
-        throw new HttpException({
-          status: HttpStatus.NOT_FOUND,
-          error: `can't get post with this id: ${postId}`,
-        }, HttpStatus.NOT_FOUND)
-      }
-      return post
-    } catch (error) {
-      throw new HttpException({
-        status: error.status || HttpStatus.INTERNAL_SERVER_ERROR,
-        error: error.response.error || `something went wrong :(`,
-      }, error.status || HttpStatus.INTERNAL_SERVER_ERROR)
+    async findUserPosts(): Promise<Post[]> {
+        try {
+            const user = this.request.user;
+            return await this.postDataBaseService.findPostsByUserId(user.id);
+        } catch (error) {
+            throw new HttpException(
+                {
+                    status: HttpStatus.INTERNAL_SERVER_ERROR,
+                    error: `something went wrong :(`,
+                },
+                HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
     }
 
-  }
-
-  async findAll(query): Promise<Post[]> {
-    try {
-      return await this.postDataBaseService.findAllPosts(query)
-    } catch (error) {
-      throw new HttpException({
-        status: error.status || HttpStatus.INTERNAL_SERVER_ERROR,
-        error: error.response.error || `something went wrong :(`,
-      }, error.status || HttpStatus.INTERNAL_SERVER_ERROR)
+    async deactivate(postId: number): Promise<Post> {
+        try {
+            const user = this.request.user;
+            const post = await this.postDataBaseService.deactivate(
+                postId,
+                user.id,
+            );
+            if (!post) {
+                throw new HttpException(
+                    {
+                        status: HttpStatus.NOT_FOUND,
+                        error: `can't get post with this id: ${postId}`,
+                    },
+                    HttpStatus.NOT_FOUND,
+                );
+            }
+            return post;
+        } catch (error) {
+            throw new HttpException(
+                {
+                    status: error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+                    error: error.response.error || `something went wrong :(`,
+                },
+                error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
     }
-  }
+
+    async update(
+        postId: number,
+        updatePostDto: UpdatePostDto,
+        images: Array<Express.Multer.File>,
+    ): Promise<Post> {
+        try {
+            const user = this.request.user;
+            const post = await this.postDataBaseService.updatePost(
+                updatePostDto,
+                postId,
+                user.id,
+                images,
+            );
+            if (!post) {
+                throw new HttpException(
+                    {
+                        status: HttpStatus.NOT_FOUND,
+                        error: `can't get post with this id: ${postId}`,
+                    },
+                    HttpStatus.NOT_FOUND,
+                );
+            }
+            return post;
+        } catch (error) {
+            throw new HttpException(
+                {
+                    status: error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+                    error: error.response.error || `something went wrong :(`,
+                },
+                error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
+    }
+
+    async remove(postId: number): Promise<Post> {
+        try {
+            const user = this.request.user;
+            const post = await this.postDataBaseService.removePost(
+                postId,
+                user.id,
+            );
+            if (!post) {
+                throw new HttpException(
+                    {
+                        status: HttpStatus.NOT_FOUND,
+                        error: `can't get post with this id: ${postId}`,
+                    },
+                    HttpStatus.NOT_FOUND,
+                );
+            }
+            return post;
+        } catch (error) {
+            throw new HttpException(
+                {
+                    status: error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+                    error: error.response.error || `something went wrong :(`,
+                },
+                error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
+    }
+
+    async findAll(query: QueryFilterDto): Promise<Post[]> {
+        try {
+            return await this.postDataBaseService.findAllPosts(query);
+        } catch (error) {
+            throw new HttpException(
+                {
+                    status: error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+                    error: error.response.error || `something went wrong :(`,
+                },
+                error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
+    }
 }
