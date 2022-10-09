4523ecf44568b7e1e172ac2d466dc49dba5abfbd
BohdanSereda <zxysereda.bodik@gmail.com>
2022-10-09 17:37:02 +0300

    formatted

--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -3,19 +3,15 @@ import { PostDataBaseService } from './posts.database.service';
 import { PostsService } from './posts.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
-import { AuthModule } from '../auth/auth.module';
 import { S3Module } from '../S3/S3.module';
-import { UsersModule } from '../users/users.module';
 
 describe('PostsService', () => {
     let postsService: PostsService;
     let postDataBaseService: PostDataBaseService;
-    const mockPostRepository ={ }
+    const mockPostRepository = {};
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            imports: [
-                S3Module,
-            ],
+            imports: [S3Module],
             providers: [
                 PostDataBaseService,
                 {
@@ -28,7 +24,7 @@ describe('PostsService', () => {
         postsService = module.get<PostsService>(PostsService);
         postDataBaseService =
             module.get<PostDataBaseService>(PostDataBaseService);
-    })
+    });
     it('should be defined', () => {
         expect(postsService).toBeDefined();
     });
