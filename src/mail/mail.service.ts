c9c98b449ae789deb6e8f04a658efa928adb864a
BohdanSereda <zxysereda.bodik@gmail.com>
2022-10-14 14:18:53 +0300

    formatted

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -4,7 +4,7 @@ import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class MailService {
-    constructor(private readonly mailerService: MailerService) {}
+    constructor(private readonly mailerService: MailerService) { }
     async sendActivationEmail(user: User): Promise<void> {
         const text = `Hi user please verify your email via link\n${process.env.API_URL}/auth/activate/${user.activationLink}`;
         const from = 'scraper.api.study@gmail.com';
